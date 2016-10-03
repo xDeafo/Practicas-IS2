a2c84b0a7b133d54a18ecda0d802d9276fcbd434
angelofky199 <angelofky199@hotmail.com>
2016-10-03 14:01:07 +0200

    Practica1 v.0.4
    Gestor y main

--- Practica1/src/GestorLiga.java
+++ Practica1/src/GestorLiga.java
@@ -14,8 +14,10 @@ public class GestorLiga {
 
     private Jugador jugador;
     private Equipo equipo;
+    private Traspaso traspaso;
     private ArrayList<Jugador> jugadores;
     private ArrayList<Equipo> equipos;
+    private ArrayList<Traspaso> traspasos;
 
     public GestorLiga(ArrayList<Jugador> jugadores, ArrayList<Equipo> equipos) {
         this.jugadores = jugadores;
@@ -33,14 +35,21 @@ public class GestorLiga {
     public void registrarEquipo(String nombre, float importeCaja, int numAbonados) {
         this.equipo = new Equipo(nombre, importeCaja, numAbonados);
     }
-    
-    public boolean añadirEquipo(Equipo e){
+
+    public boolean añadirEquipo(Equipo e) {
         return this.equipos.add(e);
     }
-    
-    public void mostrarEquipos(){
-        for(Equipo e: equipos){
+
+    public void mostrarEquipos() {
+        for (Equipo e : equipos) {
             e.toString();
         }
     }
+
+    public void mostrarTraspasos() {
+        for (Traspaso t : traspasos) {
+            t.toString();
+        }
+    }
+
 }
--- Practica1/src/Main.java
+++ Practica1/src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,19 @@
  * @author Ventura
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        String opcion;
+        Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+        opcion = (entradaEscaner.nextLine());
+        
+        
+        System.out.println("Bienvenido a la Liga BBUV");
+        
+        do{
+            System.out.println("Elige una opcion");
+            
+            
+        }while(opcion != "7");
+        
+    }
 }
