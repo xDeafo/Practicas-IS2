a82914a6429c444ae7e9a6ad9974e972550a2145
Ventura <venturaesteban3@gmail.com>
2016-10-03 13:02:10 +0200

    Practica1 v0.2
    Se implementa clase Equipo con sus atributos, constructor, getters y
    setters

--- Practica1/src/Equipo.java
+++ Practica1/src/Equipo.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,49 @@
  * @author Ventura
  */
 public class Equipo {
+    private static int CONTIDEQUIPO = 0;
+    
+    private int idEquipo;
+    private String nombre;
+    private float importeCaja;
+    private int numAbonados;
+    private ArrayList<Jugador> plantilla; 
+
+    public Equipo(String nombre, float importeCaja, int numAbonados) {
+        this.idEquipo = ++CONTIDEQUIPO;
+        this.nombre = nombre;
+        this.importeCaja = importeCaja;
+        this.numAbonados = numAbonados;
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getImporteCaja() {
+        return importeCaja;
+    }
+
+    public int getNumAbonados() {
+        return numAbonados;
+    }
+
+    public ArrayList<Jugador> getPlantilla() {
+        return plantilla;
+    }
+
+    public void setImporteCaja(float importeCaja) {
+        this.importeCaja = importeCaja;
+    }
+
+    public void setNumAbonados(int numAbonados) {
+        this.numAbonados = numAbonados;
+    }
+
+    
     
 }
