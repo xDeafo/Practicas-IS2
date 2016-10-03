e23b36a7fc15411a124e4eee1c503fce98adf38d
angelofky199 <angelofky199@hotmail.com>
2016-10-03 13:40:15 +0200

    Practica1 v.0.3
    Jugador y GestorLiga

---
+++ Practica1/src/GestorLiga.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Usuario
+ */
+public class GestorLiga {
+
+    private Jugador jugador;
+    private Equipo equipo;
+    private ArrayList<Jugador> jugadores;
+    private ArrayList<Equipo> equipos;
+
+    public GestorLiga(ArrayList<Jugador> jugadores, ArrayList<Equipo> equipos) {
+        this.jugadores = jugadores;
+        this.equipos = equipos;
+    }
+
+    public void registrarJugador(String nombre, String demarcacion, float importeClausula) {
+        this.jugador = new Jugador(nombre, demarcacion, importeClausula);
+    }
+
+    public boolean añadirJugador(Jugador j) {
+        return jugadores.add(j);
+    }
+
+    public void registrarEquipo(String nombre, float importeCaja, int numAbonados) {
+        this.equipo = new Equipo(nombre, importeCaja, numAbonados);
+    }
+    
+    public boolean añadirEquipo(Equipo e){
+        return this.equipos.add(e);
+    }
+    
+    public void mostrarEquipos(){
+        for(Equipo e: equipos){
+            e.toString();
+        }
+    }
+}
--- Practica1/src/Jugador.java
+++ Practica1/src/Jugador.java
@@ -6,28 +6,34 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Usuario
  */
 public class Jugador {
+
+    private static int CONTIDEJUGADOR = 0;
+
+    private int idJugador;
     private String nombre;
-    public enum demarcacion {portero, defensa, medio ,delantero};
+
+    public enum demarcacion {
+
+        portero, defensa, medio, delantero
+    };
     private String posicion;
     private float importeClausula;
 
-    public Jugador(String nombre, String demarcacion ,float importeClausula) {
+    public Jugador(String nombre, String demarcacion, float importeClausula) {
+        this.idJugador = ++CONTIDEJUGADOR;
         this.nombre = nombre;
         this.posicion = demarcacion;
         this.importeClausula = importeClausula;
     }
 
-
     public String getNombre() {
         return nombre;
     }
-
 
     public float getImporteClausula() {
         return importeClausula;
@@ -37,10 +43,22 @@ public class Jugador {
         this.nombre = nombre;
     }
 
-
     public void setImporteClausula(float importeClausula) {
         this.importeClausula = importeClausula;
     }
-    
+
+    @Override
+    public String toString() {
+        String result = "";
+        
+        result += "ID: " + String.format("%03d", idJugador);
+        result += " Nombre " + nombre;
+        result += " Posicion" + posicion;
+        result += " Clausula" + String.format("%.2f", importeClausula) + " €";
+        
+        return result;
+        
+        
+    }
     
 }
