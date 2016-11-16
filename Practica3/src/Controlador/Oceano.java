1085e5ded465eaedfe2c9b6adea2ea065285d33b
angelofky199 <angelofky199@hotmail.com>
2016-11-16 19:48:56 +0100

    Oceano

--- Practica3/src/Controlador/Oceano.java
+++ Practica3/src/Controlador/Oceano.java
@@ -20,6 +20,9 @@ import java.util.Random;
  */
 public class Oceano {
 
+    private static int NUM_PECES_PEQUEÑOS = 5;
+    private static int NUM_PECES_GRANDES = 5;
+    private static int NUM_DEPREDADORES = 3;
     private static int dia = 1;
     private static int temperatura = 18;
     private KrillPlacton placton;
@@ -53,110 +56,125 @@ public class Oceano {
 
         PezPequeño pp;
         int numeroPeces = Utilidades.CalculaRandom(1800, 2100);
+        int cont = 0;
 
         for (int i = 0; i <= numeroPeces; i++) {
 
             int velocidad = Utilidades.CalculaRandom(15, 35);
 
-            if (i <= numeroPeces / 5) {
-
-                pp = new PezPequeño(dia, "Boquerones", velocidad);
-                peces.add(pp);
-
-            } else if ((i > numeroPeces / 5) && (i <= numeroPeces / 5 * 2)) {
-
-                pp = new PezPequeño(dia, "Sardinas", velocidad);
-                peces.add(pp);
-
-            } else if ((i > numeroPeces / 5 * 2) && (i <= numeroPeces / 5 * 3)) {
-
-                pp = new PezPequeño(dia, "Lenguados", velocidad);
-                peces.add(pp);
-
-            } else if ((i > numeroPeces / 5 * 3) && (i <= numeroPeces / 5 * 4)) {
-
-                pp = new PezPequeño(dia, "Jureles", velocidad);
-                peces.add(pp);
-
-            } else {
-
-                pp = new PezPequeño(dia, "Gallos", velocidad);
-                peces.add(pp);
-
+            pp = crearEspeciePequeños(cont, velocidad);
+            peces.add(pp);
+            cont++;
+            if (cont % NUM_PECES_PEQUEÑOS == 0) {
+                cont = 0;
             }
+
         }
     }
-    
+
     public void crearPecesGrandes() {
 
         PezGrande pg;
         int numeroPeces = Utilidades.CalculaRandom(1800, 2100);
-
+        int cont = 0;
         for (int i = 0; i <= numeroPeces; i++) {
-            
-            int velocidad = Utilidades.CalculaRandom(40, 60);
-            
-            if (i <= numeroPeces / 5) {
 
-                pg = new PezGrande(dia, "Atunes",velocidad);
-                peces.add(pg);
+            int velocidad = Utilidades.CalculaRandom(15, 35);
 
-            } else if ((i > numeroPeces / 5) && (i <= numeroPeces / 5 * 2)) {
-
-                pg = new PezGrande(dia, "Dorados",velocidad);
-                peces.add(pg);
-
-            } else if ((i > numeroPeces / 5 * 2) && (i <= numeroPeces / 5 * 3)) {
-
-                pg = new PezGrande(dia, "Bacalaos",velocidad);
-                peces.add(pg);
-
-            } else if ((i > numeroPeces / 5 * 3) && (i <= numeroPeces / 5 * 4)) {
-
-                pg = new PezGrande(dia, "Besugos",velocidad);
-                peces.add(pg);
-
-            } else {
-
-                pg = new PezGrande(dia, "Meros",velocidad);
-                peces.add(pg);
-
+            pg = crearEspecieGrandes(cont, velocidad);
+            peces.add(pg);
+            cont++;
+            if (cont % NUM_PECES_GRANDES == 0) {
+                cont = 0;
             }
+
         }
+
     }
-    
+
     public void crearGrandesDepredadores() {
 
         GranDepredador gd;
         int numeroPeces = Utilidades.CalculaRandom(1800, 2100);
-
+        int cont = 0;
         for (int i = 0; i <= numeroPeces; i++) {
-            
+
             int velocidad = Utilidades.CalculaRandom(40, 60);
-                  
 
-            if (i <= numeroPeces / 3) {
-
-                gd = new GranDepredador(dia, "Voraces tiburones blancos");
-                peces.add(gd);
-
-            } else if ((i > numeroPeces / 3) && (i <= numeroPeces / 3 * 2)) {
-
-                gd = new GranDepredador(dia, "Tiburones toro");
-                peces.add(gd);
-
-            } else {
-
-                gd = new GranDepredador(dia, "Peces espada");
-                peces.add(gd);
-
-            }
+            gd = crearEspecieDepredadores(cont);
+            peces.add(gd);
+            cont++;
+            if (cont % NUM_DEPREDADORES) 
+                cont = 0;
+            
         }
     }
-    
+
     public void crearKrillPlancton() {
         KrillPlacton plancton = new KrillPlacton();
     }
-    
-    
+
+    public PezPequeño crearEspeciePequeños(int n, int velocidad) {
+        PezPequeño pp = null;
+        switch (n) {
+            case 1:
+                pp = new PezPequeño(dia, "Boquerones", velocidad);
+                break;
+            case 2:
+                pp = new PezPequeño(dia, "Sardinas", velocidad);
+                break;
+            case 3:
+                pp = new PezPequeño(dia, "Lenguados", velocidad);
+                break;
+            case 4:
+                pp = new PezPequeño(dia, "Jureles", velocidad);
+                break;
+            case 5:
+                pp = new PezPequeño(dia, "Gallos", velocidad);
+                break;
+
+        }
+        return pp;
+    }
+
+    public PezGrande crearEspecieGrandes(int n, int velocidad) {
+        PezGrande pg = null;
+        switch (n) {
+            case 1:
+                pg = new PezGrande(dia, "Atunes", velocidad);
+                break;
+            case 2:
+                pg = new PezGrande(dia, "Dorados", velocidad);
+                break;
+            case 3:
+                pg = new PezGrande(dia, "Bacalaos", velocidad);
+                break;
+            case 4:
+                pg = new PezGrande(dia, "Besugos", velocidad);
+                break;
+            case 5:
+                pg = new PezGrande(dia, "Meros", velocidad);
+                break;
+
+        }
+        return pg;
+    }
+
+    public GranDepredador crearEspecieDepredadores(int n) {
+        GranDepredador gd = null;
+        switch (n) {
+            case 1:
+                gd = new GranDepredador(dia, "Voraces tiburones blancos");
+                break;
+            case 2:
+                gd = new GranDepredador(dia, "Tiburones toro");
+                break;
+            case 3:
+                gd = new GranDepredador(dia, "Peces espada");
+                break;
+
+        }
+        return gd;
+    }
+
 }
